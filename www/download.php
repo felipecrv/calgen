fc0a5b48957279f5f4fa990447e627d1d692bcfd
Felipe Oliveira Carvalho <felipekde@gmail.com>
2009-07-14 21:02:34 -0300

    Melhorias em download.php

--- www/download.php
+++ www/download.php
@@ -1,4 +1,33 @@
-<h1>Código-Fonte e Binário (somente para Linux)</h1>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf8" />
+<meta name="keywords" content="calendário, gerador de calendário, calendário html, calendar generator">
+<title>Download CalGen</title>
+
+<style type="text/css">
+body {
+	font-family: "Trebuchet MS", Verdana, Arial, Helvetica, sans-serif;
+	color: #616B76;
+	background-color: #F9F8F7;
+	margin: 0;
+	padding: 20px;
+}
+
+h1 { font-size: xx-large; }
+
+a {
+	color: #F60;
+	font-weight: bold;
+};
+</style>
+
+</head>
+
+<body>
+<h1>Código-Fonte e Binário <i>(somente para Linux)</i></h1>
 
 <a href="calgen-1.0.tar.gz" title="Download">calgen-1.0.tar.gz</a>
+</body>
 
